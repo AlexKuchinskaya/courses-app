7af98e715b375abaed5359fcabb70bd955382cfe
AlexKuchinskaya <alexkuchynsk@gmail.com>
2023-09-10 18:43:41 +0200

    add handle cancel to CreateCourse

--- src/components/CreateCourse/CreateCourse.tsx
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -8,7 +8,6 @@ import React, {
 import { Input } from '../common/Input/Input';
 
 import './CreateCourse.scss';
-import { useAuthor } from '@hooks/useAuthor';
 import { useNavigate } from 'react-router-dom';
 import { ButtonTexts } from '@enums/buttonTexts';
 import { Button } from '@components/common/Button/Button';
@@ -19,24 +18,29 @@ import { useAppDispatch, useAppSelector } from '@store/utils';
 import { AuthorList } from '@components/AuthorList/AuthorList';
 import { AuthorItem } from './components/AuthorItem/AuthorItem';
 import { getCourseDuration } from '@utils/getCourseDuration';
+import { AuthorType, AuthorsList } from '@types';
+import { deleteAuthorsAction } from '@store/authors/actions';
+import { getAuthors } from '@store/authors/selectors';
+import { PathRoutes } from '@enums/pathRoutes';
 
 type CourseInput = {
-  title: string | null;
-  description: string | null;
+  title: string;
+  description: string;
   duration: number;
+  courseAuthors: AuthorsList;
 };
 
 export const CreateCourse: FC = () => {
-  const { courseAuthors } = useAuthor();
-
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const courses = useAppSelector(getCourses);
+  const authors = useAppSelector(getAuthors);
 
   const [course, setCourse] = useState<CourseInput>({
-    title: null,
-    description: null,
+    title: '',
+    description: '',
     duration: 0,
+    courseAuthors: [],
   });
 
   const [errors, setErrors] = useState({
@@ -73,7 +77,7 @@ export const CreateCourse: FC = () => {
       dispatch(
         addNewCourseAction({
           ...course,
-          authors: courseAuthors.map((el) => el.id),
+          authors: course.courseAuthors.map((el) => el.id),
         })
       );
       //navigate('/courses');
@@ -85,6 +89,61 @@ export const CreateCourse: FC = () => {
     console.log('handleDeleteAuthorFromCourseAuthors');
   };
 
+  const getCurrentAuthor = (list: AuthorsList, id: string) =>
+    list.find((author) => author.id === id);
+
+  const handleDeleteFromAuthorsList = (authorId: AuthorType['id']) => {
+    const currentAuthorCourse = getCurrentAuthor(
+      course.courseAuthors,
+      authorId
+    );
+
+    if (currentAuthorCourse) {
+      const filteredCourseAuthorsList = course.courseAuthors.filter(
+        (author) => author !== currentAuthorCourse
+      );
+      setCourse({ ...course, courseAuthors: filteredCourseAuthorsList });
+    }
+
+    dispatch(deleteAuthorsAction(authorId));
+  };
+
+  const handleAddAuthorToCourseList = (authorId: AuthorType['id']) => {
+    console.log('authorId', authorId);
+    const currentAuthorCourse = getCurrentAuthor(
+      course.courseAuthors,
+      authorId
+    );
+
+    console.log('currentAuthorCourse', currentAuthorCourse);
+
+    if (currentAuthorCourse === undefined) {
+      setCourse({
+        ...course,
+        courseAuthors: [...course.courseAuthors, currentAuthorCourse],
+      });
+    }
+    /* const isAuthorInCourse = course.courseAuthors.some(
+      (author) => author.id === authorId
+    );
+  
+    if (isAuthorInCourse) {
+      return
+    }
+    // Add the author only if they are not already in the list
+    const newAuthor = getCurrentAuthor(authors, authorId); // Assuming you have a list of authors accessible here
+    if (newAuthor) {
+      setCourse({
+        ...course,
+        courseAuthors: [...course.courseAuthors, newAuthor],
+      });
+    } */
+  };
+
+  const handleCancel = () => {
+    navigate(`/${PathRoutes.Courses}`);
+  };
+
   return (
     <div className="create-course">
       <div className="container-site">
@@ -94,57 +153,79 @@ export const CreateCourse: FC = () => {
           onSubmit={(e) => handleSubmit(e)}
         >
           <div className="create-course__form-container">
-            <h3 className="create-course__title">Main info</h3>
-            <Input
-              type="text"
-              className="create-course__input create-course__input--full-width"
-              required={true}
-              name="title"
-              value={course.title}
-              error={errors.isTitleError}
-              onChange={handleChangeInputValue}
-            />
+            <fieldset className="create-course__form-group">
+              <legend className="create-course__title">Main info</legend>
+              <Input
+                type="text"
+                className="create-course__input create-course__input--full-width"
+                required={true}
+                name="title"
+                value={course.title}
+                error={errors.isTitleError}
+                onChange={handleChangeInputValue}
+              />
+            </fieldset>
 
-            <h3 className="create-course__title">Description</h3>
-            <Textarea
-              name="description"
-              className="create-course__textarea"
-              value={course.description}
-              error={errors.isDescriptionError}
-              onChange={handleChangeTextAreaValue}
-            />
+            <fieldset className="create-course__form-group">
+              <legend className="create-course__title">Description</legend>
+              <Textarea
+                name="description"
+                className="create-course__textarea"
+                value={course.description}
+                error={errors.isDescriptionError}
+                onChange={handleChangeTextAreaValue}
+              />
+            </fieldset>
 
-            <h3 className="create-course__title">Duration</h3>
-            <Input
-              type="number"
-              className="create-course__input"
-              required={true}
-              name="duration"
-              value={course.duration}
-              error={errors.isDurationError}
-              onChange={handleChangeInputValue}
-              min={0}
-              children={
-                <div className="create-course__format-duration">
-                  {getCourseDuration(course.duration)}
-                </div>
-              }
-            />
+            <fieldset className="create-course__form-group">
+              <legend className="create-course__title">Duration</legend>
+              <Input
+                type="number"
+                className="create-course__input"
+                required={true}
+                name="duration"
+                value={course.duration}
+                error={errors.isDurationError}
+                onChange={handleChangeInputValue}
+                min={0}
+                children={
+                  <div className="create-course__format-duration">
+                    {getCourseDuration(course.duration)}
+                  </div>
+                }
+              />
+            </fieldset>
 
             <div className="create-course__authors-list">
               <div className="create-course__authors-container">
                 <AuthorList />
+                <div className="author-list__list-container">
+                  {authors.map((author) => {
+                    return (
+                      <AuthorItem
+                        key={author.id}
+                        name={author.name}
+                        onClickDeleteAuthor={() =>
+                          handleDeleteFromAuthorsList(author.id)
+                        }
+                        onClickAddAuthor={() =>
+                          handleAddAuthorToCourseList(author.id)
+                        }
+                      />
+                    );
+                  })}
+                </div>
               </div>
 
               <div className="create-course__authors-container">
                 <h3 className="create-course__title">Course Authors</h3>
                 <div className="create-course__course-authors">
-                  {courseAuthors.length ? (
-                    courseAuthors.map((courseAuthor) => {
+                  {course.courseAuthors.length ? (
+                    course.courseAuthors.map((author, id) => {
                       return (
                         <AuthorItem
-                          key={courseAuthor.id}
-                          name={courseAuthor.name}
+                          key={author.id}
+                          name={author.name}
                           onClickDeleteAuthor={
                             handleDeleteAuthorFromCourseAuthors
                           }
@@ -163,8 +244,10 @@ export const CreateCourse: FC = () => {
 
           <div className="create-course__action-btns">
             <Button
+              type="button"
               className="create-course__action-btn"
               text={ButtonTexts.Cancel}
+              onClick={handleCancel}
             />
             <Button
               type="submit"
